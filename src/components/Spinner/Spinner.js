cedf54add5e0363cbfebf2aa55fd3e93cf74b551
Sebastien Dumetz <s.dumetz@holusion.com>
2019-08-05 12:04:20 +0200

    fix spinner for wider use in custom sizes

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,10 +2,13 @@ import "./Spinner.css"
 import PropTypes from 'prop-types'
 import React from 'react'
 
-export default function Spinner({active, className, ...props}) {
+export default function Spinner({active, className, size={}, style=40, ...props}) {
     return (
-        <div className={`spinner ${className||""}${active !== false ? " active" : ""}`} {...props}>
-            <div className="spinner-content">
+        <div 
+            className={`spinner ${className||""}${active !== false ? " active" : ""}`} 
+            style={Object.assign(style, {width:size, paddingLeft: size/2})}       
+            {...props}>
+            <div className="spinner-content" style={{width:size, height: size, marginLeft: -size/2}}>
                 <span></span>
             </div>
         </div>
@@ -14,5 +17,6 @@ export default function Spinner({active, className, ...props}) {
 
 Spinner.propTypes = {
     active: PropTypes.bool,
-    absolute: PropTypes.bool
+    absolute: PropTypes.bool,
+    size: PropTypes.integer
 }
